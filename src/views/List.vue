a9d492271cb8377bcc07397e3e5896257cbbb7fd
RaphaelM <raphael.maillard@gmail.com>
2021-06-02 21:15:04 +0200

    Update front with electron

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,24 +1,20 @@
 <template>
   <div id="list">
     <div class="container-list">
-        <h1>LIVRES EN BIBLIOTHEQUE</h1>
+      <h1>LIVRES EN BIBLIOTHEQUE</h1>
 
-          <div class="row" style="margin-right:0; margin-left:6%;">
-
-            <div class="col-sm-4 " v-for="(book, bookid)  in books" :key="bookid"> 
-
-              <div class="card m-2 " style="width:75%; ">
-                <router-link :to="{name: 'Show', params: {bookId: book.id } }">
-                    <img class="card-img-top" :src= "book.image"/> 
-                </router-link> 
-                <div class="card-body">
-                  <h5 class="card-title text-center" > {{book.name}} </h5>
-                </div>
-              </div>
+      <div class="row" style="margin-right: 0; margin-left: 6%">
+        <div class="col-sm-4" v-for="(book, bookid) in books" :key="bookid">
+          <div class="card m-2" style="width: 75%">
+            <router-link :to="{ name: 'Show', params: { bookId: book.id } }">
+              <img class="card-img-top" :src="book.image" />
+            </router-link>
+            <div class="card-body">
+              <h5 class="card-title text-center">{{ book.name }}</h5>
             </div>
-              
           </div>
-
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -27,10 +23,9 @@
 import axios from "axios";
 
 export default {
-  
   data: () => {
     return {
-      books:[]
+      books: [],
     };
   },
   mounted() {
@@ -45,107 +40,90 @@ export default {
       });
   },
 };
-
 </script>
 
 <style scoped>
-@media  screen and (max-width: 675px) {
-@import url('https://fonts.googleapis.com/css2?family=Roboto');
+@media screen and (max-width: 675px) {
+  @import url("https://fonts.googleapis.com/css2?family=Roboto");
 
-h1{
-  text-align: center;
-  font-size:20px;
-  font-weight: 900;
-  letter-spacing: 3.5px;
-  padding:  10%;
-  text-decoration: overline;
-  font-family: Roboto;
-  letter-spacing: 3px;
-  color: black;
- 
-}
+  h1 {
+    text-align: center;
+    font-size: 20px;
+    font-weight: 900;
+    letter-spacing: 3.5px;
+    padding: 10%;
+    text-decoration: overline;
+    font-family: Roboto;
+    letter-spacing: 3px;
+    color: black;
+  }
 
- 
   .container-list {
     padding-left: 25%;
-    padding-top:45%;
-    
+    padding-top: 45%;
   }
 
-  .display-card{
+  .display-card {
     width: 80%;
     z-index: 0;
-   
   }
 
-.card-title{
-  font-size: 15px;
+  .card-title {
+    font-size: 15px;
+  }
 }
 
-}
+@media screen and (min-width: 1224px) {
+  @import url("https://fonts.googleapis.com/css2?family=Roboto");
 
-@media screen and (min-width:1224px){
+  h1 {
+    text-align: center;
+    font-size: 40px;
+    font-weight: 900;
+    letter-spacing: 5px;
+    text-decoration: overline;
+    font-family: Roboto;
+    margin: 5%;
+    color: black;
+  }
 
-@import url('https://fonts.googleapis.com/css2?family=Roboto');
-
-h1{
-  text-align: center;
-  font-size:40px;
-  font-weight: 900;
-  letter-spacing: 5px;
-  text-decoration: overline;
-  font-family: Roboto;
-  margin: 5%;
-  color: black;
-  
-}
-
- .container-list {
+  .container-list {
     margin-left: 15%;
-    
   }
 
-   .display-card{
+  .display-card {
     width: 20%;
     z-index: 0;
   }
-
-  
 }
 
-  @media screen and (min-width: 676px) and (max-width: 1223px){
-
-      @import url("https://fonts.googleapis.com/css2?family=Roboto");
+@media screen and (min-width: 676px) and (max-width: 1223px) {
+  @import url("https://fonts.googleapis.com/css2?family=Roboto");
 
   * a:hover {
-    text-decoration:none;
+    text-decoration: none;
   }
 
-   
-h1{
-  text-align: center;
-  font-size:25px;
-  font-weight: 900;
-  letter-spacing: 3.5px;
-  padding:  25%;
-  text-decoration: overline;
-  font-family: Roboto;
-  letter-spacing: 3px;
-  color: black;
- 
-}
+  h1 {
+    text-align: center;
+    font-size: 25px;
+    font-weight: 900;
+    letter-spacing: 3.5px;
+    padding: 25%;
+    text-decoration: overline;
+    font-family: Roboto;
+    letter-spacing: 3px;
+    color: black;
+  }
 
- 
   .container-list {
     padding-left: 25%;
-    padding-top:15%;
-    
+    padding-top: 15%;
   }
 
-  .display-card{
+  .display-card {
     width: 40%;
     z-index: 0;
   }
-  }
+}
 </style>
-
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,186 +1,220 @@
 <template>
   <div id="register">
     <div class="container-register">
+      <h1>S'INSCRIRE</h1>
 
-        <h1>S'INSCRIRE</h1> 
+      <div class="container-form">
+        <form v-on:submit.prevent="submitForm">
+          <div class="form-group input-group">
+            <div class="input-group-prepend">
+              <span class="input-group-text"> <i class="fa fa-user"></i> </span>
+            </div>
+            <input
+              name=""
+              class="form-control col-md-4"
+              id="firstname"
+              placeholder="first name"
+              type="text"
+              v-model="form.firstname"
+            />
+          </div>
+          <!-- form-group// -->
+          <div class="form-group input-group">
+            <div class="input-group-prepend">
+              <span class="input-group-text"> <i class="fa fa-user"></i> </span>
+            </div>
+            <input
+              name=""
+              class="form-control col-md-4"
+              id="lastname"
+              placeholder="last name"
+              type="text"
+              v-model="form.lastname"
+            />
+          </div>
+          <!-- form-group// -->
+          <!-- <div class="form-group input-group">
+            <div class="input-group-prepend">
+              <span class="input-group-text"> <i class="fa fa-envelope"></i> </span>
+            </div>
+            <input
+              name=""
+              class="form-control col-md-4"
+              id="email"
+              placeholder="Email"
+              type="email"
+              v-model="form.email"
+            />
+          </div> -->
+          <!-- form-group// -->
 
-       <div class="container-form">
-          <form v-on:submit.prevent="submitForm">
-            <div class="form-group input-group ">
-                <div class="input-group-prepend ">
-                    <span class="input-group-text"> <i class="fa fa-user"></i> </span>
-                </div>
-                <input name="" class="form-control col-md-4 " id="firstname" placeholder="first name" type="text" v-model="form.firstname">
-            </div> <!-- form-group// -->
-            <div class="form-group input-group">
-                <div class="input-group-prepend">
-                    <span class="input-group-text"> <i class="fa fa-user"></i> </span>
-                </div>
-                <input name="" class="form-control col-md-4" id="lastname" placeholder="last name" type="text" v-model="form.lastname">
-            </div> <!-- form-group// -->
-            <div class="form-group input-group">
-                <div class="input-group-prepend">
-                    <span class="input-group-text"> <i class="fa fa-envelope"></i> </span>
-                </div>
-                <input name="" class="form-control col-md-4" id="email" placeholder="Email" type="email" v-model="form.email">
-            </div> <!-- form-group// -->
-            
-           
-            <div class="form-group input-group">
-                <div class="input-group-prepend">
-                    <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
-                </div>
-                <input class="form-control col-md-4" id="password" placeholder="mot de passe" type="password" v-model="form.password">
-            </div> <!-- form-group// -->
-            <div class="form-group input-group">
-                <div class="input-group-prepend">
-                    <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
-                </div>
-                <input class="form-control col-md-4" id="repassword" placeholder="confirmer mot de passe" type="password" v-model="form.repassword">
-            </div> <!-- form-group// -->                                      
-            <div class="form-group">
-                <button type="submit" class="btn btn-dark btn-block col-md-2"> Créer un compte  </button>
-            </div> <!-- form-group// -->  
-                
-            <p class="text-center">Déja utilisateur ? <router-link to="/login"><a href="">Se connecter</a> </router-link></p>  
-                                                                           
+          <div class="form-group input-group">
+            <div class="input-group-prepend">
+              <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
+            </div>
+            <input
+              class="form-control col-md-4"
+              id="password"
+              placeholder="mot de passe"
+              type="password"
+              v-model="form.password"
+            />
+          </div>
+          <!-- form-group// -->
+          <div class="form-group input-group">
+            <div class="input-group-prepend">
+              <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
+            </div>
+            <input
+              class="form-control col-md-4"
+              id="repassword"
+              placeholder="confirmer mot de passe"
+              type="password"
+              v-model="form.repassword"
+            />
+          </div>
+          <!-- form-group// -->
+          <div class="form-group">
+            <button type="submit" class="btn btn-dark btn-block col-md-2">
+              Créer un compte
+            </button>
+          </div>
+          <!-- form-group// -->
+
+          <p class="text-center">
+            Déja utilisateur ?
+            <router-link to="/login"><a href="">Se connecter</a> </router-link>
+          </p>
         </form>
-        </div>
-
-     </div>
-
-
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-  
-    data(){
-        return{
-            form: {
-                firstname: '',
-                lastname:'',
-                email:'',
-                password:'',
-                repassword:''
-                
-                
-            }
-        }
-    },
+  data() {
+    return {
+      form: {
+        firstname: "",
+        lastname: "",
+        // email: "",
+        password: "",
+        // repassword:'',
+        birthday: "2021-02-16T00:00:00+00:00",
+        username: "",
+      },
+    };
+  },
 
-    methods:{
-        submitForm(){
-            axios.post('http://127.0.0.1:8000/api', this.form)
-                .then((res) => {
-                this.form = res.data;
-                console.log(this.form);
-              })
-              .catch((error) => {
-                console.log(error);
-              });
-          },
-        }
-        };             
-          
-   
+  methods: {
+    submitForm() {
+      axios
+        .post("http://127.0.0.1:8000/api/users", this.form)
+        .then((res) => {
+          this.form = res.data;
+          console.log(this.form);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    //   submitForm() {
+    //     axios({
+    //       method: "post",
+    //       url: "http://127.0.0.1:8000/api/users",
+    //       data: {
+    //         username: this.firstname,
+    //         firstname: this.firstname,
+    //         lastname: this.lastname,
+    //         password: this.password,
+    //         birthday: "2021-02-16T00:00:00+00:00",
+
+    //         username: "test",
+    //         password: "test",
+    //         firstname: "test",
+    //         lastname: "test",
+    //         birthday: "2021-02-16T00:00:00+00:00",
+    //       },
+    //     });
+    //   },
+  },
+};
 </script>
 
-
 <style scoped>
-@media  screen and (max-width: 675px) {
-@import url('http://fonts.googleapis.com/css2?family=Roboto');
+@media screen and (max-width: 675px) {
+  @import url("http://fonts.googleapis.com/css2?family=Roboto");
 
-h1{
-  text-align: center;
-  font-size:20px;
-  font-weight: 900;
-  letter-spacing: 3.5px;
-  padding:  10%;
-  text-decoration: overline;
-  font-family: Roboto;
-  letter-spacing: 3px;
-  color: black;
- 
-}
+  h1 {
+    text-align: center;
+    font-size: 20px;
+    font-weight: 900;
+    letter-spacing: 3.5px;
+    padding: 10%;
+    text-decoration: overline;
+    font-family: Roboto;
+    letter-spacing: 3px;
+    color: black;
+  }
 
-
- 
   .container-register {
     padding-left: 25%;
-    padding-top:45%;
-    
+    padding-top: 45%;
   }
 
-   .container-form {
-margin: 5%;
-    
-    
+  .container-form {
+    margin: 5%;
+  }
+}
+
+@media screen and (min-width: 1224px) {
+  @import url("https://fonts.googleapis.com/css2?family=Roboto");
+
+  h1 {
+    text-align: center;
+    font-size: 30px;
+    font-weight: 900;
+    letter-spacing: 5px;
+    text-decoration: overline;
+    font-family: Roboto;
+    margin: 5%;
+    color: black;
   }
 
-
-}
-
-@media screen and (min-width:1224px){
-
-@import url('https://fonts.googleapis.com/css2?family=Roboto');
-
-h1{
-  text-align: center;
-  font-size:30px;
-  font-weight: 900;
-  letter-spacing: 5px;
-  text-decoration: overline;
-  font-family: Roboto;
-  margin: 5%;
-  color: black;
-  
-}
-
-
- .container-register {
+  .container-register {
     margin-left: 12.5%;
-    
   }
 
- .container-form {
-    margin-left:35%; 
-    
+  .container-form {
+    margin-left: 35%;
   }
 }
 
-  @media screen and (min-width: 676px) and (max-width: 1223px){
-
-      @import url("https://fonts.googleapis.com/css2?family=Roboto");
+@media screen and (min-width: 676px) and (max-width: 1223px) {
+  @import url("https://fonts.googleapis.com/css2?family=Roboto");
 
   * a:hover {
-    text-decoration:none;
+    text-decoration: none;
   }
 
-  
-h1{
-  text-align: center;
-  font-size:30px;
-  font-weight: 900;
-  letter-spacing: 2.5px;
-  padding:  10%;
-  text-decoration: overline;
-  font-family: Roboto;
-  
-  color: black;
- 
-}
+  h1 {
+    text-align: center;
+    font-size: 30px;
+    font-weight: 900;
+    letter-spacing: 2.5px;
+    padding: 10%;
+    text-decoration: overline;
+    font-family: Roboto;
 
+    color: black;
+  }
 
   .container-register {
     padding-left: 35%;
-    padding-top:30%;
-    
+    padding-top: 30%;
   }
-
 }
 </style>
-
